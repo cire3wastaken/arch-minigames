90cbf36aa41504e13ba1e3e5a03c2ea13cb79bd5
Michele <michelegje@gmail.com>
2026-06-07 05:40:38 +0200

    Remove debug msgs

--- shared/src/main/kotlin/gg/tropic/practice/queue/QueueCommunications.kt
+++ shared/src/main/kotlin/gg/tropic/practice/queue/QueueCommunications.kt
@@ -91,7 +91,6 @@ object QueueCommunications
                     ?.score
                     ?: 1000
 
-                println("[lobby-queue] publishing join packet: kit=${kit.id} type=${queueType.name} size=${teamSize} leader=${player.uniqueId}")
                 createMessage(
                     packet = "join",
                     "entry" to QueueEntry(
@@ -110,7 +109,6 @@ object QueueCommunications
                     context = AwareThreadContext.SYNC,
                     channel = "communications-gamequeue"
                 )
-                println("[lobby-queue] published join packet for ${player.name}")
             }
             .exceptionally {
                 player.sendMessage("${CC.RED}We were unable to put you in the queue!")
